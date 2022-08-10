449f5444f81fe799a7d33e8831dd4d6a01c17aa2
Nicklas NNIT <nimu@nnit.com>
2022-08-10 14:58:14 +0200

    Finishes basic text classification

--- basic_text_classification/main.py
+++ basic_text_classification/main.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import os
 import re
 import string
+import matplotlib.pyplot as plt
 
 if not os.path.exists('aclImdb'):
     # Alternatively just use: 
@@ -15,6 +16,7 @@ if not os.path.exists('aclImdb'):
 
 dataset_dir = 'aclImdb'
 train_dir = os.path.join(dataset_dir, 'train')
+test_dir = os.path.join(dataset_dir, 'test')
 unsupervised_traindir = os.path.join(train_dir, 'unsup')
 
 if os.path.exists(unsupervised_traindir):
@@ -37,6 +39,11 @@ raw_val_ds = tf.keras.utils.text_dataset_from_directory(
     subset='validation',
     seed=seed)
 
+raw_test_ds = tf.keras.utils.text_dataset_from_directory(
+    test_dir,
+    batch_size=batch_size
+)
+
 
 def custom_standardization(input_data):
   lowercase = tf.strings.lower(input_data)
@@ -46,12 +53,101 @@ def custom_standardization(input_data):
                                   '')
 
 max_features = 10_000
-sequence_length = 20
+sequence_length = 250
 
 vectorize_layer = tf.keras.layers.TextVectorization(
-    standardize = custom_standardization,
-    max_tokens = max_features,
-    output_mode = 'int',
-    output_sequence_length = sequence_length)
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
 
-# https://www.tensorflow.org/tutorials/keras/text_classification#prepare_the_dataset_for_training
+train_text = raw_train_ds.map(lambda x,y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+  text = tf.expand_dims(text, -1)
+  return vectorize_layer(text), label
+
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+embedding_dim = 16
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(
+        max_features+1, embedding_dim),
+        #Input dim,     #Output dim
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.GlobalAveragePooling1D(), # What does this do?
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1)
+])
+
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer='adam',
+              metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
+
+
+loss, accuracy = model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+history_dict = history.history
+history_dict.keys()
+
+acc = history_dict['binary_accuracy']
+val_acc = history_dict['val_binary_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# # "bo" is for "blue dot"
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# # b is for "solid blue line"
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.legend()
+
+# plt.savefig('Training_loss.png')
+# plt.close()
+
+# plt.plot(epochs, acc, 'bo', label='Training acc')
+# plt.plot(epochs, val_acc, 'b', label='Validation acc')
+# plt.title('Training and validation accuracy')
+# plt.xlabel('Epochs')
+# plt.ylabel('Accuracy')
+# plt.legend(loc='lower right')
+
+# plt.savefig('Training_acc.png')
+
+export_model = tf.keras.Sequential([
+    vectorize_layer,
+    model,
+    tf.keras.layers.Activation('sigmoid')
+])
+
+export_model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=False), optimizer='adam', metrics=['accuracy']
+)
+
+loss, accuracy = export_model.evaluate(raw_test_ds)
+print(accuracy)
+
